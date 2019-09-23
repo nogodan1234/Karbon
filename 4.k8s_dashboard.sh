929a29d3a0134599c2a23d75d925c6aeb57301c4
nogodan1234 <choi.taeho@gmail.com>
2019-09-23 17:47:19 +1000

    Updated Readme.md

--- 4.k8s_dashboard.sh
+++ 4.k8s_dashboard.sh
@@ -5,7 +5,7 @@ kubectl create serviceaccount dashboard -n default
 echo "Creating Cluster Role Binding for the dashboard"
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
 echo "Getting the token info for Dashboard"
-kubectl get secret $(kubectl get serviceaccount dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
+kubectl get secret $(kubectl get serviceaccount dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode | echo 
 #kubectl describe secrets $(kubectl get secrets | grep -i default | awk -F" " '{print $1}') | grep token | grep -v Name | grep -v Type
 #pending on 
 #https://github.com/helm/charts/issues/9776
