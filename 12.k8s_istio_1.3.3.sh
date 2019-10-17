32719799740aa14a4d15624fb1de0f8331fc707d
nogodan1234 <choi.taeho@gmail.com>
2019-10-17 16:35:48 +1100

    Adding grafana for Istio access

--- 12.k8s_istio_1.3.3.sh
+++ 12.k8s_istio_1.3.3.sh
@@ -9,3 +9,5 @@ sleep 20
 kubectl patch svc istio-ingressgateway -n istio-system  -p '{"spec": {"type": "NodePort"}}'
 kubectl label namespace default istio-injection=enabled
 kubectl get namespace -L istio-injection
+echo "Redirecting port to Grafana UI access"
+kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &
