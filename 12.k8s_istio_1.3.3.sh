d9d3ad5436df35906b347586fbe1785c0552fe0d
nogodan1234 <choi.taeho@gmail.com>
2019-10-17 16:33:13 +1100

    Updating istio 1.3.3 for new ns

--- 12.k8s_istio_1.3.3.sh
+++ 12.k8s_istio_1.3.3.sh
@@ -1,12 +1,11 @@
 helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.3.3/charts/
-helm install istio.io/istio-init
+kubectl create ns istio-system
+helm install istio.io/istio-init --namespace istio-system
 sleep 20
-helm install istio.io/istio-cni
+helm install istio.io/istio-cni --namespace istio-system
 sleep 20
-helm install istio.io/istio
+helm install istio.io/istio --set grafana.enabled=true --namespace istio-system
 sleep 20
-kubectl patch svc istio-ingressgateway   -p '{"spec": {"type": "NodePort"}}'
+kubectl patch svc istio-ingressgateway -n istio-system  -p '{"spec": {"type": "NodePort"}}'
 kubectl label namespace default istio-injection=enabled
-sleep 20
-echo "Check which namespace is enabled for Istio injection"
 kubectl get namespace -L istio-injection
