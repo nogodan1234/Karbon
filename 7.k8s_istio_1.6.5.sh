86dbf103d4e08f889b39e52811d06f4fa3aee1a8
nogodan1234 <choi.taeho@gmail.com>
2020-08-27 21:24:23 +1000

    Istio updated

--- 7.k8s_istio_1.6.5.sh
+++ 7.k8s_istio_1.6.5.sh
@@ -4,15 +4,12 @@ curl -L https://istio.io/downloadIstio | sh -
 export PATH=$PWD/bin:$PATH
 echo "Installing istio for demo profile base on your current kubeconfig file...."
 istioctl install --set profile=demo
-echo "Label default namespace to inject Envoy sidecar for Istio"
+echo "Label default namespace to inject Envoy sidecar"
 kubectl label namespace default istio-injection=enabled
-#echo "Patching the service to NodePort by assuming LB is not available"
-#kubectl patch svc istio-ingressgateway -n istio-system  -p '{"spec": {"type": "NodePort"}}'
+echo "Patching the service to NodePort"
+kubectl patch svc istio-ingressgateway -n istio-system  -p '{"spec": {"type": "NodePort"}}'
 kubectl get namespace -L istio-injection
-echo "Redirecting port to Grafana UI access"
-kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &
-
-#How to delete istio from the cluster
-#istioctl manifest generate --set profile=demo | kubectl delete -f -
-#kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
-#kubectl patch svc kiali -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
+echo "Patching grafana,kiali,jaeger service to NodePort"
+kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "NodePort"}}'
+kubectl patch svc kiali -n istio-system -p '{"spec": {"type": "NodePort"}}'
+kubectl patch svc jaeger-query  -n istio-system -p '{"spec": {"type": "NodePort"}}'
